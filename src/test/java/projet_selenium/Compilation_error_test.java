7d6c3af0d899453c1006034e8915123f8ededf54
Kasia <90092402+kklepacka@users.noreply.github.com>
2023-04-03 16:26:48 +0200

    Update and rename SampleTest_08.java to Compilation_error_test.java
    modify test to generate a test compilation

---
+++ src/test/java/projet_selenium/Compilation_error_test.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class ExampleTest {
+    
+    @Test
+    public void testAddition() {
+        int a = 2;
+        int b = 3;
+        int result = a + b;
+        assertEquals(5, result);
+    }
+    
+    @Test
+    public void testSubtraction() {
+        int a = 5;
+        int b = 2;
+        int result = a - b
+        assertEquals(3, result);
+    }
+}
